b4eb03947c28b15cee284388a3802cb5e2124aff
502-15\cre <maihei541@gmail.com>
2025-09-10 12:10:08 +0900

    [Add]SearchInformation

--- Team2OpenDataApp/src/Team2OpenDataApp.cpp
+++ Team2OpenDataApp/src/Team2OpenDataApp.cpp
@@ -2,12 +2,15 @@
 #include <iostream>
 #include <string>
 #include "System.h"
+#include "information.h"
 
 int main()
 {
 	SetConsoleOutputCP(CP_UTF8);  // 出力コードページをUTF-8に変更
 	std::u8string str = u8"こんにちは、世界";
 	printUtf8(str);
+
+	ShowSearchInformation();//案内の表示
 
 	system("pause");
 	return 0;
