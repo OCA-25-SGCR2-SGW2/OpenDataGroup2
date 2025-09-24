acb38f1b2ae9a2bce3760e874e10fbdd51290e66
502-16\cre <a>
2025-09-24 12:04:40 +0900

    ファイルを追加

---
+++ Team2OpenDataApp/src/Prefectures.cpp
@@ -0,0 +1,24 @@
+//	mainで打たれた都道府県名を引っ張ってくる
+//　データが入っている関数からデータを引っ張ってくる
+//　両方のデータを比較する
+//　一致すれば出す
+#include "Prefectures.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+bool Prefectures::Init() {
+	count = 0;
+
+	return true;
+}
+
+void Prefectures::Updat() {
+
+
+}
+
+void Prefectures::FindByName()
+{
+
+}
---
+++ Team2OpenDataApp/src/Prefectures.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+
+class Prefectures {
+private:
+
+	std::string name;
+
+	int count;
+
+public:
+	bool Init();
+
+	void Updat();
+
+	void FindByName();
+
+
+
+
+};
