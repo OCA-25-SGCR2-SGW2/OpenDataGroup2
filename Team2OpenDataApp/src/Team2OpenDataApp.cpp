9bfacfea16fd74c317b15f592712b5bc5db4d2a7
502-15\cre <maihei541@gmail.com>
2025-10-29 10:00:29 +0900

    [Add]DeleteFavoriteList

--- Team2OpenDataApp/src/Team2OpenDataApp.cpp
+++ Team2OpenDataApp/src/Team2OpenDataApp.cpp
@@ -37,7 +37,7 @@ int main()
 		ShowInformation("option_info");//案内表示
 		int option;
 		std::cin >> option;
-		while ((option > 7 || option < 1) || std::cin.fail()) {
+		while ((option > 8 || option < 1) || std::cin.fail()) {
 			std::cin.clear();
 			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 			std::cout << "不正な入力です。正しい値を入力してください。\n\n";
@@ -106,15 +106,106 @@ int main()
 		}
 		break;
 		case 5:
+			//ブックマークの削除
+		{
+			while (true) {
+				auto fav_data = favavorites_list.GetFavoritesData();//お気に入り情報を取得
+				//番号付きで表示
+				size_t fav_data_size = fav_data.size();//お気に入りデータの個数
+				//絞り込んだ結果を表示
+				if (fav_data.empty()) {
+					//該当するデータがない場合のメッセージ
+					printUtf8(u8"--------------------");
+					std::u8string no_results_message = u8"該当するデータが見つかりませんでした。\nブックマーク削除オプションを終了します。";
+					printUtf8(no_results_message);
+					printUtf8(u8"--------------------\n\n");
+					break;
+				}
+				else {
+					printUtf8(u8"--------------------");
+					std::u8string results_message = u8"お気に入りは" + std::u8string(reinterpret_cast<const char8_t*>(std::to_string(fav_data.size()).c_str())) + u8"件あります。";
+					printUtf8(results_message);
+					for (const auto& entry : fav_data) {
+						printUtf8(u8"--------------------\n");
+						printUtf8(u8"番号: " + std::u8string(reinterpret_cast<const char8_t*>(std::to_string(&entry - &fav_data[0] + 1).c_str())) + u8"\n");
+						for (const auto& [key, value] : entry) {
+							std::u8string output = u8"" + std::u8string(key.begin(), key.end()) + u8": " + value + u8"\n";
+							printUtf8(output);
+						}
+					}
+					printUtf8(u8"--------------------\n\n");
+				}
+				ShowInformation("bookmark_delete_info");//案内表示
+				bool exit_flag = false;//削除処理を終了するかどうかのフラグ
+				size_t delete_idx;
+				std::cin >> delete_idx;
+				//例外処理
+				while ((delete_idx < 1 || delete_idx > fav_data_size) || std::cin.fail()) {
+					//-1なら終了したい
+					if (delete_idx == -1) {
+						exit_flag = true;//削除処理を終了する。
+						break;
+					}
+					std::cin.clear();
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+					std::u8string error_message = u8"無効な番号です。1から" + std::u8string(reinterpret_cast<const char8_t*>(std::to_string(fav_data_size).c_str())) + u8"の数字を入力してください。\n終了する:-1";
+					printUtf8(error_message);
+					std::cin >> delete_idx;
+				}
+				//削除処理を終了するなら抜ける
+				if (exit_flag) {
+					printUtf8(u8"--------------------");
+					std::u8string exit_message = u8"-1が入力されました。\nブックマーク削除オプションを終了します。";
+					printUtf8(exit_message);
+					printUtf8(u8"--------------------\n\n");
+					break;
+				}
+				//対応するデータを削除
+				if (favavorites_list.DeleteFavorite(delete_idx - 1)) {
+					//お気に入りリストの保存
+					{
+						std::ofstream os("Data/FavoritesList.json"); // ファイルに書き込む
+						cereal::JSONOutputArchive archive(os);
+						archive(favavorites_list); // オブジェクトを保存
+					}
+					std::u8string success_message = u8"指定された番号のブックマークを削除しました。\n";
+					printUtf8(success_message);
+				}
+				else {
+					std::u8string failure_message = u8"指定された番号のブックマークの削除に失敗しました。\n";
+					printUtf8(failure_message);
+				}
+				//もう一度削除するかどうかを確認
+				std::u8string prompt_message = u8"別の項目も削除しますか？\n削除する:0\n終わる:1";
+				printUtf8(prompt_message);
+				int continue_option;
+				std::cin >> continue_option;
+				//例外処理
+				while ((continue_option < 0 || continue_option > 1) || std::cin.fail()) {
+					std::cin.clear();
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+					std::u8string error_message = u8"無効なオプションです。0か1の数字を入力してください。\n削除する:0\n終わる:1";
+					printUtf8(error_message);
+					std::cin >> continue_option;
+				}
+				//1ならループを抜ける
+				if (continue_option == 1) {
+					break;
+				}
+				//0なら続行
+			}
+		}
+		break;
+		case 6:
 			//ブックマークの表示
 			favavorites_list.ShowAllFavorites();	//お気に入りを表示
 			break;
-      case 6:
-        //人口比率分析
+		case 7:
+			//人口比率分析
 			PopulationAnalysis::DisplayPopulationRatioAnalysis();
 			break;
-        break;
-		case 7:
+			break;
+		case 8:
 			//終了
 			return 0;
 			break;
